dae080c7dacc73d94e40803430d4d7327ae765b0
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-02-27 13:45:18 +0200

    Update autounattend.ps1

--- autounattend.ps1
+++ autounattend.ps1
@@ -15,6 +15,21 @@ New-Item Sources -ItemType Directory | Out-Null
 Copy-Item -Path "$Drive\*" -Destination Sources -Recurse -Force
 
 irm https://github.com/GabiNun/autounattend/raw/main/autounattend.xml -Out Sources\autounattend.xml
+
+attrib -r C:\Users\User\Sources\sources\install.esd
+
+New-Item Install -ItemType Directory | Out-Null
+
+Mount-WindowsImage -ImagePath Sources\sources\install.esd -Index 6 -Path Install | Out-Null
+
+Foreach ($Package in (Get-AppxProvisionedPackage -Path Install).PackageName | Where-Object { $_ -notmatch 'DesktopAppInstaller|SecHealthUI' }) {
+  Remove-ProvisionedAppPackage -Path Install -PackageName $Package | Out-Null
+}
+
+
+
+
+
 irm https://msdl.microsoft.com/download/symbols/oscdimg.exe/688CABB065000/oscdimg.exe -Out oscdimg.exe
 
 .\oscdimg.exe "-bSources\efi\microsoft\boot\efisys.bin" -u2 Sources autounattend.iso
