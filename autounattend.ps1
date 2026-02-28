c413b0eec0c19d041124f82148948be89edc4e7c
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-02-28 09:35:05 +0200

    Update autounattend.ps1

--- autounattend.ps1
+++ autounattend.ps1
@@ -15,7 +15,7 @@ New-Item Sources -ItemType Directory | Out-Null
 Copy-Item -Path "$Drive\*" -Destination Sources -Recurse -Force
 
 irm https://github.com/GabiNun/autounattend/raw/main/autounattend.xml -Out Sources\autounattend.xml
-irm https://msdl.microsoft.com/download/symbols/oscdimg.exe/688CABB065000/oscdimg.exe -Out oscdimg.exe
+irm https://msdl.microsoft.com/download/symbols/oscdimg.exe/688CABB065000/oscdimg.exe -Out oscdimg.exe # I got this url from `winget install oscdimg`
 
 .\oscdimg.exe "-bSources\efi\microsoft\boot\efisys.bin" -u2 Sources autounattend.iso
 
